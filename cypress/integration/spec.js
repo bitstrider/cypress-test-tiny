b4e94265279d94e3f03ebaf2505adbfa24fbcce0
Jason Yung <json.yung@gmail.com>
2018-12-14 17:14:24 -0500

    Added simple spec to demo feature request
    - want to create the same test for each item in an array that was created asynchronously

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,46 @@
-describe('page', () => {
-  it('works', () => {})
-})
+// @ts-check
+/* eslint-env mocha */
+
+describe("todos API", () => {
+
+  const initialTags = ["knowledge", "practice", "fun"];
+
+  const initialTodos = [
+    {
+      id: 1,
+      tag: "knowledge",
+      task: "read something"
+    },
+    {
+      id: 2,
+      tag: "practice",
+      task: "write something"
+    }
+  ];
+
+  let TAGS;
+
+  before(() => {
+
+    // set up request stubbing
+    cy.server()
+    cy.route("GET", "/tags", initialTags)
+    cy.route("GET", "/todos/by-tags/*", initialTodos)
+
+    // request tags async from remote server
+    cy.request("/tags")
+      .then(tags => TAGS = tags)
+
+  });
+
+  // test for each tag retrieved
+  TAGS.forEach(tag => {
+
+    it(`returns todos for tag '${tag}'`, () => {
+      cy.request(`/todos/by-tag/${tag}`)
+        .its("body")
+        .should("be.an", "array");
+    });
+    
+  });
+});
